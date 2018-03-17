c6b105552196d74636d7d1e56cccc4deb030da9d
mottox2 <y.mottox2@gmail.com>
2018-03-17 11:12:15 +0900

    Add ability to create todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,95 @@
 import * as React from 'react'
 import styled from 'styled-components'
+import { defineMode } from 'codemirror'
+import { Controlled as CodeMirror } from 'react-codemirror2'
+
+import Todo from './Todo'
+
+const Container = styled.div`
+  padding: 16px;
+`
 
 const Title = styled.h1`
   font-size: 22px;
 `
 
 class App extends React.Component {
+  state = {
+    value: '',
+    todos: [
+      {
+        number: 1,
+        title: 'Initial TODO',
+        category: 'default/',
+        tags: ['#sample']
+      }
+    ]
+  }
+
+  instance: any
+
   render() {
-    return <Title>Welcome to React</Title>
+    return (
+      <Container>
+        <Title>Todo List</Title>
+        <CodeMirror
+          editorDidMount={(editor: any) => {
+            this.instance = editor
+            this.instance.setSize(null, 40)
+            editor.setSize(null, editor.defaultTextHeight() + 2 * 4)
+          }}
+          autoFocus={true}
+          editorWillMount={() => {
+            defineMode('custom', () => {
+              return {
+                token: (stream: any, state: any) => {
+                  if (stream.match(/#\w+/)) {
+                    return 'keyword'
+                  }
+                  if (stream.match(/\w+\//)) {
+                    return 'comment'
+                  }
+                  stream.next()
+                  return null
+                }
+              }
+            })
+          }}
+          options={{
+            mode: 'custom'
+          }}
+          onKeyDown={(_editor, e: any) => {
+            if (e.keyCode === 13 && this.state.value.length > 0) {
+              // console.log(this.state.value)
+              this.setState({
+                todos: [
+                  {
+                    number: this.state.todos[0].number + 1,
+                    tags: this.state.value.match(/#\w+/g) || [],
+                    category: (this.state.value.match(/\w+\//g) || []).join(''),
+                    title: this.state.value.replace(/#\w+/g, '').replace(/\w+\//g, '')
+                  },
+                  ...this.state.todos
+                ],
+                value: ''
+              })
+            }
+          }}
+          value={this.state.value}
+          onBeforeChange={(editor, change, value) => {
+            // var newtext = change.text.join('').replace(/\n/g, '')
+            // change.update(change.from, change.to, [newtext])
+            this.setState({ value: value.replace(/\n/g, '') })
+            // this.setState({ value: value.replace(/\w+\//g, '') })
+            return true
+          }}
+          onChange={(editor, data, value) => {
+            // console.log(editor, data, value)
+          }}
+        />
+        {this.state.todos.map(todo => <Todo key={todo.number} todo={todo} />)}
+      </Container>
+    )
   }
 }
 
---
+++ src/Todo.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react'
+import styled from 'styled-components'
+
+const Todo: React.StatelessComponent<{
+  className?: string
+  todo?: any
+}> = ({ todo, className }: any) => (
+  <div className={className}>
+    {todo.category && <div className="category">{todo.category}</div>}
+    <div className="title">{todo.title}</div>
+    {todo.tags.map((tag: string, i: number) => (
+      <div className="tag" key={i}>
+        {tag}
+      </div>
+    ))}
+  </div>
+)
+
+export default styled(Todo)`
+  margin: 12px 0;
+  padding-bottom: 12px;
+  border-bottom: 1px solid #e5e5e5;
+  animation: fadeIn 0.4s ease-out;
+  .category {
+    opacity: 0.8;
+    font-size: 12px;
+  }
+  .title {
+    font-weight: bold;
+    display: inline-block;
+  }
+
+  .tag {
+    display: inline-block;
+    font-size: 12px;
+    /* background-color: #ddd; */
+    border-radius: 2px;
+    margin-right: 4px;
+    /* padding: 3px 6px; */
+    &:before {
+      content: ' ';
+      margin-left: 4px;
+    }
+  }
+`
