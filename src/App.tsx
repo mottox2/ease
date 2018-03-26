21aeaf8e1c319a82e42e78fe3b83ee2f93a28a2c
mottox2 <y.mottox2@gmail.com>
2018-03-27 02:11:42 +0900

    Tweak style

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,39 @@ import TaskItem from './components/TaskItem'
 import TaskInput from './components/TaskInput'
 
 const Container = styled.div`
-  padding: 16px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+`
+
+const Sidebar = styled.div`
+  width: 260px;
+  padding: 40px;
+`
+
+const SidebarItem = styled.div`
+  padding: 12px 12px;
+  opacity: 0.88;
+  font-size: 15px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    background-color: #f0f0f0;
+  }
+  small {
+    margin-left: 4px;
+    font-family: Helvetica, sans-serif;
+    opacity: 0.66;
+  }
+`
+
+const Main = styled.div`
+  width: 600px;
+  background-color: #fff;
+  border-right: 1px solid #eee;
+  border-left: 1px solid #eee;
+  padding: 40px;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.12);
 `
 
 const Title = styled.h1`
@@ -40,11 +72,19 @@ class App extends React.Component<
   }
 
   render() {
+    const { tasks } = this.state
     return (
       <Container>
-        <Title>Todo List</Title>
-        <TaskInput addTask={this.addTask} />
-        {this.state.tasks.map((task: Task, index: number) => <TaskItem key={index} task={task} />)}
+        <Sidebar>
+          <SidebarItem>
+            すべてのタスク<small>{tasks.length}</small>
+          </SidebarItem>
+        </Sidebar>
+        <Main>
+          <Title>Ease Todo</Title>
+          <TaskInput addTask={this.addTask} />
+          {tasks.map((task: Task, index: number) => <TaskItem key={index} task={task} />)}
+        </Main>
       </Container>
     )
   }
